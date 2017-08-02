8e93f3b74f092507d6cb4156fdb0516b1c4aa392
Alexander Loginov <alexander.s.loginov@gmail.com>
2017-08-02 23:09:34 +0300

    [02/08/2017] RestController is added

--- sql/sjblog.sql
+++ sql/sjblog.sql
@@ -1,43 +1,49 @@
 CREATE TABLE IF NOT EXISTS users (
-	user_id SERIAL NOT NULL AUTO_INCREMENT,
+	user_id SERIAL,
 	nickname VARCHAR(50) NOT NULL,
 	PRIMARY KEY (user_id)
 ) Engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS records (
-	rec_id SERIAL NOT NULL AUTO_INCREMENT,
-	user_id SERIAL NOT NULL,
+	rec_id SERIAL,
+	user_id BIGINT UNSIGNED NOT NULL,
 	rec_title VARCHAR(200) NOT NULL,
 	rec_body TEXT NOT NULL,
 	crt_date TIMESTAMP NOT NULL,
 	mdf_date TIMESTAMP NOT NULL,
+	INDEX u_id (user_id),
 	PRIMARY KEY (rec_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id)
+	CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (user_id)
+	    ON DELETE CASCADE ON UPDATE CASCADE
 ) Engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS comments (
-	comment_id SERIAL NOT NULL AUTO_INCREMENT,
-	user_id SERIAL NOT NULL,
-	rec_id SERIAL NOT NULL,
+	comment_id SERIAL,
+	user_id BIGINT UNSIGNED NOT NULL,
+	rec_id BIGINT UNSIGNED NOT NULL,
 	comment_body TEXT NOT NULL,
 	crt_date TIMESTAMP NOT NULL,
 	mdf_date TIMESTAMP NOT NULL,
+	INDEX u_id(user_id),
+	INDEX r_id(rec_id),
 	PRIMARY KEY (comment_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id),
-	FOREIGN KEY (rec_id) REFERENCES records (rec_id)
+	CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY (rec_id) REFERENCES records (rec_id) ON DELETE CASCADE
 ) Engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS tags (
-	tag_id SERIAL NOT NULL AUTO_INCREMENT,
+	tag_id SERIAL,
 	tag_name VARCHAR(50) NOT NULL,
 	PRIMARY KEY (tag_id)
 ) Engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS tag_rec_ref (
-	tr_id SERIAL NOT NULL AUTO_INCREMENT,
-	tag_id SERIAL NOT NULL,
-	rec_id SERIAL NOT NULL,
+	tr_id SERIAL,
+	tag_id BIGINT UNSIGNED NOT NULL,
+	rec_id BIGINT UNSIGNED NOT NULL,
+	INDEX t_id(tag_id),
+	INDEX r_id(rec_id),
 	PRIMARY KEY (tr_id),
-	FOREIGN KEY (tag_id) REFERENCES tags (tag_id),
-	FOREIGN KEY (rec_id) REFERENCES records (rec_id)
+	CONSTRAINT FOREIGN KEY (tag_id) REFERENCES tags (tag_id) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY (rec_id) REFERENCES records (rec_id) ON DELETE CASCADE
 ) Engine=InnoDB;
--- src/main/java/org/sbx/sjblog/controllers/MainViewController.java
+++ src/main/java/org/sbx/sjblog/controllers/MainViewController.java
@@ -4,31 +4,46 @@ import org.sbx.sjblog.entity.impl.Record;
 import org.sbx.sjblog.service.ObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class MainViewController {
 
     @Autowired
     @Qualifier("recordService")
-    private ObjectService service;
+    private ObjectService<Integer, Record> service;
 
-    @RequestMapping(name = "/", method = RequestMethod.GET)
-    public String showAll(ModelMap map) throws SQLException {
-
+    @RequestMapping(value = "/record/", method = RequestMethod.GET)
+    public ResponseEntity<List<Record>> getAllRecords() throws SQLException {
         List<Record> records = service.getAll();
+        if (records.isEmpty()) {
+            return new ResponseEntity<List<Record>>(HttpStatus.NO_CONTENT);
+        }
 
-        map.addAttribute("records", records);
+        return new ResponseEntity<List<Record>>(records, HttpStatus.OK);
+    }
 
-        return "response";
+    @RequestMapping(value = "/record/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Record> getRecord(@PathVariable("id") int id) throws SQLException {
+
+        Record record = service.findById(id);
+
+        if (record == null) {
+            return new ResponseEntity<Record>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<Record>(record, HttpStatus.OK);
 
     }
 
